6eb6fc3ff071ebafdbbf214117c786fa1ddcdbeb
Casey Hyde <casey@fundkit.io>
2022-03-09 15:13:13 -0600

    Initial Commit

---
+++ src/main/java/io/fidgetlabs/samplegradleapp3/SampleGradleApp3Application.java
@@ -0,0 +1,13 @@
+package io.fidgetlabs.samplegradleapp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGradleApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGradleApp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/fidgetlabs/samplegradleapp3/SampleGradleApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fidgetlabs.samplegradleapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGradleApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
